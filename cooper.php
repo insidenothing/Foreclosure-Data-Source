469b8b69460c9453a1833bb274344684d4f07b58
Patrick McGuire <insidenothing@gmail.com>
2011-11-05 23:22:08 -0400

    ads

--- cooper.php
+++ cooper.php
@@ -5,6 +5,17 @@ mysql_select_db('mfg');
 <style>
 td { font-size:10pt; }
 </style>
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-2410355655106377";
+/* mfg page */
+google_ad_slot = "8350166361";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
 <table>
 	<tr>
 		<td>Date Online</td>
